7229131c7faaf2df67484f90cac0b2810678d82c
Sabdadon <sabarish111095@gmail.com>
2017-05-19 14:12:22 +0530

    Created the modal class and cosntants

---
+++ weatherApp/constants.swift
@@ -0,0 +1,15 @@
+//
+//  constants.swift
+//  weatherApp
+//
+//  Created by sabarish sridhar on 19/05/17.
+//  Copyright © 2017 sabarish. All rights reserved.
+//
+
+import Foundation
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APPID = "&appid="
+let APIKEY = "243f6117cfb66470c1b92b091d8bff82"
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)20\(LONGITUDE)100\(APPID)\(APIKEY)"
---
+++ weatherApp/currentWeather.swift
@@ -0,0 +1,57 @@
+//
+//  currentWeather.swift
+//  weatherApp
+//
+//  Created by sabarish sridhar on 19/05/17.
+//  Copyright © 2017 sabarish. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class currentWeather {
+
+    var _cityName : String!
+    var _date :String!
+    var _weatherType : String!
+    var _currentTemp : Double!
+    
+    var cityName :String{
+    if _cityName == nil
+    {
+        _cityName = ""
+        }
+        return _cityName
+    }
+    
+    var weatherType :String{
+        if _weatherType == nil{
+        _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    var currentTemp :Double{
+        if _currentTemp == nil{
+        _currentTemp = 0.0
+        }
+        return _currentTemp
+    }
+    
+    var date : String{
+    
+        if _date == nil{
+        
+        _date = ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        dateFormatter.timeStyle = .none
+        let currentDate = dateFormatter.string(from: Date())
+        self._date = "Today,\(currentDate)"
+        
+return _date
+    }
+}
+
