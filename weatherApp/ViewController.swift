b4c53ffe171f6a40be90332fc23d5ed9361b12b7
Sabdadon <sabarish111095@gmail.com>
2017-05-19 11:40:18 +0530

    Added Information View

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
 
+    @IBOutlet weak var datelbl: UILabel!
+    
+    @IBOutlet weak var InfoView: UITableView!
+    @IBOutlet weak var currentWeatherType: UILabel!
+    @IBOutlet weak var currentWeatherImg: UIImageView!
+    @IBOutlet weak var cityLbl: UILabel!
+    @IBOutlet weak var currentTempLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        InfoView.dataSource = self
+        InfoView.delegate = self
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+    
+    
 
 }
 
